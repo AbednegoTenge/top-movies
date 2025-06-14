d0081d953f0b65f697fb3a8f2ac0ad17aa91a1be
AbednegoTenge <abednegotenge180@gmail.com>
2025-06-14 21:59:29 +0000

    setting up movie modle

---
+++ database/movieDb.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import mongooseSequence from 'mongoose-sequence';
+
+const AutoIncrement = mongooseSequence(mongoose);
+
+
+const MovieSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    rating: { type: Number, min: 0, max: 10 },
+    ranking: Number,
+    review: String,
+    year: String,
+    description: String,
+    image_url: String
+})
+
+// Applying autoincrement on id
+MovieSchema.plugin(AutoIncrement, { inc_field: '_id' });
+
+export default mongoose.model('Movies', MovieSchema);
