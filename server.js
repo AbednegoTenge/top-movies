571dbb4f7d6e270279aa42857ea38b1167eacf09
AbednegoTenge <abednegotenge180@gmail.com>
2025-06-14 21:58:14 +0000

    setting p movie model

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = process.env.PORT
 
+const PORT = process.env.PORT;
+const mongoUrl = process.env.MONGOURL;
 
-app.listen(PORT, () => {
-    console.log(`Server running on 'http://localhost:${PORT}`)
-});
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected');
+
+    app.listen(PORT, () => {
+        console.log(`Server running on 'http://localhost:${PORT}`)
+    });
+}).catch(err => console.error('MongoDB connection error:', err));
