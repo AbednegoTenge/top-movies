35437ef14107545fd23fe86b86876549d19761f3
AbednegoTenge <abednegotenge180@gmail.com>
2025-06-24 16:53:02 +0000

    integrating delete route into authRoute

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -3,10 +3,12 @@ import authController from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.get('/', authController.home)
-router.get('/add', authController.addMoviePage)
-router.post('/add', authController.addMovieSearch)
-router.get('/edit/:id', authController.editMoviePage)
-router.post('/edit/:id', authController.editMovieForm)
+router.get('/', authController.home);
+router.get('/add', authController.addMoviePage);
+router.post('/add', authController.addMovieSearch);
+router.get('/edit/:id', authController.editMoviePage);
+router.post('/edit/:id', authController.editMovieForm);
+router.delete('/delete/:id', authController.delete);
+
 
 export default router;
